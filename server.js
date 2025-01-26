f38a8a29d9b1099fc892af5cc3301dd072c4ac4f
ClaudioOner <claudio.cortes@marval.cl>
2025-01-26 15:50:29 -0300

    Primer commit

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mysql = require('mysql2');
 const cors = require('cors');
 require('dotenv').config();  // Para usar las variables de entorno
+const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -22,40 +23,22 @@ db.connect(err => {
   }
 });
 
-// Ruta de prueba
+// Ruta de la API para obtener los datos desde la base de datos
 app.get('/api/data', (req, res) => {
-    db.query('SELECT * FROM usuarios', (err, result) => {  
-      if (err) {
-        console.error('Database query error:', err);  // Log de error con más detalles
-        res.status(500).send('Database error');
-      } else {
-        console.log('Database query result:', result);  // Log del resultado de la consulta
-        res.json(result);
-      }
-    });
-  });
-  
-
-  db.connect(err => {
+  db.query('SELECT * FROM usuarios', (err, result) => {
     if (err) {
-      console.error('Error connecting to the database:', err.stack);
+      console.error('Database query error:', err);  // Log de error con más detalles
+      res.status(500).send('Database error');
     } else {
-      console.log('Connected to the MySQL database');
+      console.log('Database query result:', result);  // Log del resultado de la consulta
+      res.json(result);
     }
   });
-  
-
-
-
-// Iniciar el servidor
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
 });
 
-const path = require('path');
-
+// Enviar los archivos estáticos de React solo cuando estemos en producción
 if (process.env.NODE_ENV === 'production') {
-  // Sirve los archivos estáticos de React
+  // Sirve los archivos estáticos de React desde la carpeta build
   app.use(express.static(path.join(__dirname, 'client', 'build')));
 
   // Redirige todas las rutas no definidas a la aplicación React
@@ -63,3 +46,8 @@ if (process.env.NODE_ENV === 'production') {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
+
+// Iniciar el servidor
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
